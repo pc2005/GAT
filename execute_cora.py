110ccbff9379199b5b7a4eaf640fed52ad69b310
pc2005 <philchen2005@gmail.com>
2021-01-16 19:51:54 -0500

    Added more comments.

--- execute_cora.py
+++ execute_cora.py
@@ -37,19 +37,21 @@ print('residual: ' + str(residual))
 print('nonlinearity: ' + str(nonlinearity))
 print('model: ' + str(model))
 
-### Load data ###
+# Load data
 adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = process.load_data(
     dataset)
 
-### Feature extraction ###
+# Feature vector normalization
 features, spars = process.preprocess_features(features)
 
 nb_nodes = features.shape[0]
 ft_size = features.shape[1]
 nb_classes = y_train.shape[1]
 
+# adjacent matrix
 adj = adj.todense()
 
+# add batch dimension
 features = features[np.newaxis]
 adj = adj[np.newaxis]
 y_train = y_train[np.newaxis]
@@ -62,6 +64,7 @@ test_mask = test_mask[np.newaxis]
 biases = process.adj_to_bias(adj, [nb_nodes], nhood=1)
 
 with tf.Graph().as_default():
+    # build input data_dict
     with tf.name_scope('input'):
         ftr_in = tf.placeholder(dtype=tf.float32, shape=(
             batch_size, nb_nodes, ft_size))
@@ -74,21 +77,31 @@ with tf.Graph().as_default():
         ffd_drop = tf.placeholder(dtype=tf.float32, shape=())
         is_train = tf.placeholder(dtype=tf.bool, shape=())
 
+    # build network
     logits = model.inference(ftr_in, nb_classes, nb_nodes, is_train,
                              attn_drop, ffd_drop,
                              bias_mat=bias_in,
                              hid_units=hid_units, n_heads=n_heads,
                              residual=residual, activation=nonlinearity)
+
+    # tile to remove batch dimension                       
     log_resh = tf.reshape(logits, [-1, nb_classes])
     lab_resh = tf.reshape(lbl_in, [-1, nb_classes])
     msk_resh = tf.reshape(msk_in, [-1])
+
+    # build loss op
     loss = model.masked_softmax_cross_entropy(log_resh, lab_resh, msk_resh)
+
+    # build accuracy op
     accuracy = model.masked_accuracy(log_resh, lab_resh, msk_resh)
 
+    # build training op
     train_op = model.training(loss, lr, l2_coef)
 
+    # build checkpoint saver op
     saver = tf.train.Saver()
 
+    # build init op
     init_op = tf.group(tf.global_variables_initializer(),
                        tf.local_variables_initializer())
 
@@ -97,6 +110,7 @@ with tf.Graph().as_default():
     curr_step = 0
 
     with tf.Session() as sess:
+        # init
         sess.run(init_op)
 
         train_loss_avg = 0
@@ -104,9 +118,10 @@ with tf.Graph().as_default():
         val_loss_avg = 0
         val_acc_avg = 0
 
+        # training and validating
         for epoch in range(nb_epochs):
-            tr_step = 0
-            tr_size = features.shape[0]
+            tr_step = 0                 # step counter
+            tr_size = features.shape[0] #! always same as batch size?
 
             # forward training
             while tr_step * batch_size < tr_size:
@@ -147,6 +162,7 @@ with tf.Graph().as_default():
                 if val_acc_avg/vl_step >= vacc_mx and val_loss_avg/vl_step <= vlss_mn:
                     vacc_early_model = val_acc_avg/vl_step
                     vlss_early_model = val_loss_avg/vl_step
+                    # save checkpoint with better performance
                     saver.save(sess, checkpt_file)
                 vacc_mx = np.max((val_acc_avg/vl_step, vacc_mx))
                 vlss_mn = np.min((val_loss_avg/vl_step, vlss_mn))
@@ -165,13 +181,16 @@ with tf.Graph().as_default():
             val_loss_avg = 0
             val_acc_avg = 0
 
+        # load best checkpoint
         saver.restore(sess, checkpt_file)
 
+        # Run test set
         ts_size = features.shape[0]
         ts_step = 0
         ts_loss = 0.0
         ts_acc = 0.0
 
+        tic = time.time()
         while ts_step * batch_size < ts_size:
             loss_value_ts, acc_ts = sess.run([loss, accuracy],
                                              feed_dict={
@@ -186,6 +205,6 @@ with tf.Graph().as_default():
             ts_step += 1
 
         print('Test loss:', ts_loss/ts_step,
-              '; Test accuracy:', ts_acc/ts_step)
+              '; Test accuracy:', ts_acc/ts_step, '; Run time (s):', (time.time()-tic))
 
         sess.close()
